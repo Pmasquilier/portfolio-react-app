44f8fc9a279e96f1903a7e91e72e3aa4ef435193
Pierre Masquilier <pierremasquilier@gmail.com>
2022-09-22 19:42:22 -0300

    ajout du formulaire de contact

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
@@ -12,6 +13,7 @@ function App() {
       <About></About>
       <Skills></Skills>
       <Portfolio></Portfolio>
+      <Contact></Contact>
     </div>
   );
 }
