34a068fdeb8e319aadb074161dd92099b4bb60d0
Pierre Masquilier <pierremasquilier@gmail.com>
2022-10-25 19:42:07 -0300

    add lotterry dapp

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,71 +1,121 @@
-import React from 'react'
-import Guidemichmich from '../assets/guidemichmich.jpg';
-import PortfolioImg from '../assets/portfolio.jpg';
+import React from "react";
+import Guidemichmich from "../assets/guidemichmich.jpg";
+import PortfolioImg from "../assets/portfolio.jpg";
+import LotteryDapp from "../assets/lottery-dapp.jpg";
 
 function Portfolio() {
   return (
-    <div name='portfolio' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Portfolio</p>
-                <p className='py-6 '>Voici un aperçu de mes récents projets</p>
-            </div>
-
-            {/* Guide mich mich */}
-            <div 
-            className='grid sm:grid-cols-2 md:grid-cols-2 gap-4'>
-                <div 
-                style={{ backgroundImage: `url(${Guidemichmich})` }}
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/* Hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                    <span className='text-2xl font-bold text-white tracking-wider'>
-                        Site web en Angular
-                    </span>
-                    <div className='pt-8 text-center'>
-                        <a href='https://www.guidemichmich.com/#/' target="_blank" rel="noreferrer">
-                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                            Demo
-                        </button>
-                        </a>
-                        <a href='https://github.com/Pmasquilier/les_jeans_michs_en_voyage' target="_blank" rel="noreferrer">
-                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                            Code
-                        </button>
-                        </a>
-                    </div>
-                </div>
-              </div>
-
-                {/* Portfolio */}
-              <div 
-                style={{ backgroundImage: `url(${PortfolioImg})` }}
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/* Hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                    <span className='text-2xl font-bold text-white tracking-wider'>
-                        Site web en ReactJS
-                    </span>
-                    <div className='pt-8 text-center'>
-                        <a href='https://www.pierremasquilier.com/' target="_blank" rel="noreferrer">
-                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                            Demo
-                        </button>
-                        </a>
-                        <a href='https://github.com/Pmasquilier/portfolio-react-app' target="_blank" rel="noreferrer">
-                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                            Code
-                        </button>
-                        </a>
-                    </div>
-                </div>
-              </div>
-            </div>
+    <div
+      name="portfolio"
+      className="w-full md:h-screen text-gray-300 bg-[#0a192f]"
+    >
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Portfolio
+          </p>
+          <p className="py-6 ">Voici un aperçu de mes récents projets</p>
         </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
+          <div
+            style={{ backgroundImage: `url(${Guidemichmich})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Notre tour du monde - Angular
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://www.guidemichmich.com/#/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/Pmasquilier/les_jeans_michs_en_voyage"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${PortfolioImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Mon Portfolio - ReactJS
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://www.pierremasquilier.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/Pmasquilier/portfolio-react-app"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${LotteryDapp})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Lottery dapp - NextJS - ThirdWeb
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://next-lottery-dapp.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/Pmasquilier/lottery-dapp"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
